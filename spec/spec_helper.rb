7c60c41298928e40358da2f16f1216dd5481f8cc
Thomas Dmytryk <tdmytryk@fanhattan.com>
2013-05-26 20:19:20 -0700

    Test runs on the cloud

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 require File.dirname(__FILE__) + '/../lib/exodus'
 Dir["#{File.dirname(__FILE__)}/support/*.rb"].each { |f| require f }
+mongo_uri = 'mongodb://exodus:exodus@dharma.mongohq.com:10048/Exodus-test'
 
 Exodus.configure do |config| 
-	config.db = 'database_test'
-	config.connection = Mongo::MongoClient.new("127.0.0.1", 27017, :pool_size => 5, :pool_timeout => 5)
+	config.db = 'Exodus-test'
+	config.connection = Mongo::MongoClient.from_uri(mongo_uri)
 	config.config_file = File.dirname(__FILE__) + '/support/config.yml'
 end
 
