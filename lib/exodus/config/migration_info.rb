110229d50972e89c8cf35ecd7d2ed8fc64daf165
Thomas Dmytryk <tdmytryk@fanhattan.com>
2013-11-24 18:32:21 -0800

    Added rake namespace to avoid conflict with other gems

--- lib/exodus/config/migration_info.rb
+++ lib/exodus/config/migration_info.rb
@@ -1,7 +1,7 @@
 module Exodus
 	class MigrationInfo
 		attr_accessor :info, :migrations_directory
-		attr_reader :config_file, :db, :connection
+		attr_reader :config_file, :db, :connection, :rake_namespace
 
 		def initialize(file = nil)
 			config_file = file if file 
@@ -18,10 +18,15 @@ module Exodus
 		def config_file=(file)
 			if File.exists?(file)
 				@config_file = file
-				@info = YAML.load_file(file)
+				self.info = YAML.load_file(file)
+				self.rake_namespace = (info['migration'] && info['migration']['rake_namespace']).to_s
 			else
 				raise ArgumentError, "#{file} not found"
 			end
+		end
+
+		def rake_namespace=(namespace)
+			@rake_namespace = namespace.to_s.empty? || namespace[-1] == ':' ? namespace.to_s : namespace + ':'
 		end
 
 		def migrate
@@ -52,7 +57,7 @@ module Exodus
 			begin
 				yield if block_given?
 			rescue
-				Raise StandardError, "Syntax error detected in config file #{self.config_file}. To find the good syntax take a look at the documentation."
+				Raise StandardError, "Syntax error detected in config file #{self.config_file}. To find the correct syntax take a look at the documentation."
 			end
 		end
 	end
